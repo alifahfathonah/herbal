9adf0c4f7460a4b819088f987b51bfde71c15697
faristoriqu <44657860+faristoriqu@users.noreply.github.com>
2019-05-11 09:10:49 +0700

    showhide angular
    masih belum crud

--- application/views/_partials/script.php
+++ application/views/_partials/script.php
@@ -39,8 +39,8 @@
 <!-- Angular -->
 <script src="<?php echo base_url('assets/')?>dist/js/angular.js"></script>
 <!-- DataTables -->
-<script src="../../bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
-<script src="../../bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
+<script src="<?php echo base_url('assets/')?>bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
+<script src="<?php echo base_url('assets/')?>bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
 <script>
   $(function () {
     $('#example1').DataTable()
