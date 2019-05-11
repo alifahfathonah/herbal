456f3cd31f8ef483aa14a502b1df2048d2dd999d
Yusufandi99 <44702492+Yusufandi99@users.noreply.github.com>
2019-05-11 14:08:18 +0700

    Dashboard
    Tampilannya

--- application/views/_partials/sidebar.php
+++ application/views/_partials/sidebar.php
@@ -12,19 +12,48 @@
       
       <ul class="sidebar-menu" data-widget="tree">
         
-        <li class="active treeview">
+        <li class="treeview">
           <a href="#">
-            <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+            <i class="fa fa-dashboard"></i> <span>Home</span>
             <span class="pull-right-container">
               <i class="fa fa-angle-left pull-right"></i>
             </span>
           </a>
           <ul class="treeview-menu">
-            <li class="active"><a href="<?php echo site_url('User')?>"><i class="fa fa-circle-o"></i> Register</a></li>
-            <li><a href="index2.html"><i class="fa fa-circle-o"></i> Dashboard v2</a></li>
+              <li><a href="index2.html"><i class="fa fa-circle-o"></i> Data Barang</a></li>
+              <li><a href="index2.html"><i class="fa fa-circle-o"></i> Data Barang Masuk</a></li>
+              <li><a href="index2.html"><i class="fa fa-circle-o"></i> Data Pelanggan</a></li>
+              <li><a href="index2.html"><i class="fa fa-circle-o"></i> Transaksi</a></li>
+              <li><a href="index2.html"><i class="fa fa-circle-o"></i> Pemesanan</a></li>
+          </ul>
+        </li>
+       <li class="treeview">
+          <a href="#">
+            <i class="fa fa-folder"></i>
+            <span>Menu</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+          <li><a href="pages/charts/chartjs.html"><i class="fa fa-circle-o"></i> Login</a></li>
+          <li class="active"><a href="<?php echo site_url('User')?>"><i class="fa fa-circle-o"></i> Register</a></li>
           </ul>
         </li>
         <li class="treeview">
+          <a href="#">
+            <i class="fa fa-book"></i>
+            <span>Laporan</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="pages/UI/general.html"><i class="fa fa-circle-o"></i> Pemesanan </a></li>
+            <li><a href="pages/UI/icons.html"><i class="fa fa-circle-o"></i> Transaksi </a></li>
+            </ul>
+        </li>
+         <!-- <li class="treeview">
           <a href="#">
             <i class="fa fa-files-o"></i>
             <span>Layout Options</span>
@@ -47,39 +76,8 @@
             </span>
           </a>
         </li>
-        <li class="treeview">
-          <a href="#">
-            <i class="fa fa-pie-chart"></i>
-            <span>Charts</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="pages/charts/chartjs.html"><i class="fa fa-circle-o"></i> ChartJS</a></li>
-            <li><a href="pages/charts/morris.html"><i class="fa fa-circle-o"></i> Morris</a></li>
-            <li><a href="pages/charts/flot.html"><i class="fa fa-circle-o"></i> Flot</a></li>
-            <li><a href="pages/charts/inline.html"><i class="fa fa-circle-o"></i> Inline charts</a></li>
-          </ul>
-        </li>
-        <li class="treeview">
-          <a href="#">
-            <i class="fa fa-laptop"></i>
-            <span>UI Elements</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="pages/UI/general.html"><i class="fa fa-circle-o"></i> General</a></li>
-            <li><a href="pages/UI/icons.html"><i class="fa fa-circle-o"></i> Icons</a></li>
-            <li><a href="pages/UI/buttons.html"><i class="fa fa-circle-o"></i> Buttons</a></li>
-            <li><a href="pages/UI/sliders.html"><i class="fa fa-circle-o"></i> Sliders</a></li>
-            <li><a href="pages/UI/timeline.html"><i class="fa fa-circle-o"></i> Timeline</a></li>
-            <li><a href="pages/UI/modals.html"><i class="fa fa-circle-o"></i> Modals</a></li>
-          </ul>
-        </li>
-        <li class="treeview">
+         -->
+        <!-- <li class="treeview">
           <a href="#">
             <i class="fa fa-edit"></i> <span>Forms</span>
             <span class="pull-right-container">
@@ -123,7 +121,7 @@
             </span>
           </a>
         </li>
-       
+        -->
         
       </ul>
     </section>
