bab20554bcdbd6e294fb77cf684e5d279cadb60e
khizam19 <19izambgb>
2019-07-06 22:50:35 +0700

    oioi
    sip

--- application/controllers/Laporan_Barang.php
+++ application/controllers/Laporan_Barang.php
@@ -1,48 +1,65 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Laporan_Barang extends CI_Controller {
-    
-    function __construct(){
+class Laporan_Barang extends CI_Controller
+{
+
+	function __construct()
+	{
 		parent::__construct();
 		$this->load->model("M_Laporan_Barang");
 		$this->load->library('form_validation');
 		$this->load->helper('url');
-		if($this->session->userdata('status') != 'login'){
-				redirect(base_url(""));
+		if ($this->session->userdata('status') != 'login') {
+			redirect(base_url(""));
 		}
 	}
-	public function index(){
-		$data['data']=$this->db->get('barang')->result();
+	public function index()
+	{
 		$this->load->view("laporan_barang/laporan.php");
 	}
+	public function kategoriQ()
+	{
+		$this->load->view("laporan_barang/cetak/cetak_barang.php");
+	}
 
-	public function getAll(){	
-		$data=$this->M_Laporan_Barang->ambil_data();
+	public function getAll()
+	{
+		$data = $this->M_Laporan_Barang->ambil_data();
 		echo json_encode($data);
 	}
 
-	public function getWhere($kategori){	
+	public function getkategori()
+	{
+		$data["laporan"] = $this->M_Laporan_Barang->getAll();
+		$this->load->view('laporan_barang/cetak/cetak_barang', $data);
+	}
+
+	public function getWhere($kategori)
+	{
 		// $data=$this->M_Laporan_Barang->ambil_data();
 		$data = $this->db->query("SELECT * FROM barang WHERE kategori = '$kategori'")->result();
 		echo json_encode($data);
 	}
 
-	function kategori(){
+	function kategori()
+	{
 		$kategori = $_POST['kategori'];
-			$data['data'] = $this->model->get_kategori($kategori);		
-			$this->load->view('admin/produk/list2',$data);
+		$data['data'] = $this->model->get_kategori($kategori);
+		$this->load->view('laporan_barang/cetak/cetak_barang', $data);
 	}
 
-	public function cetak(){
-		$data['laporan_barang'] = $this->M_Laporan_Barang->view_row();
-		$this->load->view('print',$data);
+	public function cetak()
+	{
+		$data["laporan"] = $this->M_Laporan_Barang->ambil_data();
+		ob_start();
+		$this->load->view('laporan_barang/cetak/cetak_barang', $data);
 		$html = ob_get_contents();
-		 	ob_end_clean();
 
-		 	require_once('./assets/html2pdf/html2pdf.class.php');
-		 $pdf = new HTML2PDF('P','A4','en');    
-		 $pdf->WriteHTML($html);   
-		 $pdf->Output('Laporan Barang.pdf', 'D');
+		ob_end_clean();
+		require_once('./assets/html2pdf/html2pdf.class.php');
+		$pdf = new HTML2PDF('P', 'A4', 'en');
+		$pdf->WriteHTML($html);
+		$pdf->Output('Data Barang.pdf', 'D');
 	}
 }
--- application/models/M_Laporan_Barang.php
+++ application/models/M_Laporan_Barang.php
@@ -1,23 +1,36 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class M_Laporan_Barang extends CI_Model {
+class M_Laporan_Barang extends CI_Model
+{
     // deklarasi variable
     private $_table = "barang";
+    private $_table1 = "laporan_";
+
 
     //menampilkan data
-    public function rule(){
-        return[
-            [`field` => `nama`,
-            `rules` => `required`],
-            [`field` => `password`,
-            `rules` => `required`],
+    public function rule()
+    {
+        return [
+            [
+                `field` => `nama`,
+                `rules` => `required`
+            ],
+            [
+                `field` => `password`,
+                `rules` => `required`
+            ],
         ];
     }
 
     //menampilkan data
-    public function ambil_data(){
+    public function ambil_data()
+    {
         return $this->db->get($this->_table)->result();
+    }
+    public function ambil_datakategori()
+    {
+        return $this->db->get($this->_table1)->result();
     }
 
     // public function getDataByKategori()
